2f2285e751e8ac9ed8baa191df3c7002338c76ae
jarp <tuemail@ejemplo.com>
2026-05-28 00:28:00 -0400

    usuario con permiso

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //if (!User::where('email', 'admin@ejemplo.com')->exists()) {
+        $role = Role::firstOrCreate(['name' => 'admin']);
+
+        // 2. Creamos o buscamos al usuario
+        $user = User::firstOrCreate(
+            ['email' => 'admin@ejemplo.com'],
+            [
+                'name' => 'Administrador General',
+                'password' => Hash::make('password_segura_123'),
+            ]
+        );
+
+        // 3. Asignamos el rol al usuario
+        $user->assignRole($role);
+    }
+}
