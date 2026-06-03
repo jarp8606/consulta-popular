aa1fee7c0d2198649a88a31845e275be6faa02fc
jarp <tuemail@ejemplo.com>
2026-06-02 21:34:28 -0400

    Añadidas  api bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php', // <--- ¡AQUÍ ESTÁ LO QUE FALTA!
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
